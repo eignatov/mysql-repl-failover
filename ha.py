42bcec0be22b048166032a1d63b68e7dfad94148
utada <utada4@gmail.com>
2012-07-04 19:42:21 +0900

    modified:   ha.py

--- ha.py
+++ ha.py
@@ -1,7 +1,7 @@
 #!/opt/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2012, utada4@gmail.com, https://github.com/utada/mysql-repl-failover
+# Copyright (c) 2012, UTADA katsuya
 # 
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -30,11 +30,13 @@ from MySQLdb import cursors
 import commands
  
 def conn_master1():
+  ### edit environment
   host     = '127.0.0.1'
   port     = '3306'
   username = 'root'
   password = ''
   dbname   = 'mysql'
+  ###
   return MySQLdb.connect(host=host,
                          db=dbname,
                          user=username,
@@ -44,11 +46,13 @@ def conn_master1():
                          cursorclass = MySQLdb.cursors.SSCursor)
 
 def conn_master2():
+  ### edit environment
   host     = '127.0.0.1'
   port     = '3307'
   username = 'root'
   password = ''
   dbname   = 'mysql'
+  ###
   return MySQLdb.connect(host=host,
                          db=dbname,
                          user=username,
@@ -105,17 +109,17 @@ def start_slave(conn):
 def failover1():
   # when master1 down
   sys.stderr.write("failover1\n")
-  commands.getoutput("supervisorctl stop mysql-proxy-master1")
-  commands.getoutput("supervisorctl stop mysql-proxy-master2")
-  commands.getoutput("supervisorctl stop mysql-proxy-slave1")
-  commands.getoutput("supervisorctl stop mysql-proxy-slave2")
+  commands.getoutput("supervisorctl stop mysql-proxy-master1_1")
+  commands.getoutput("supervisorctl stop mysql-proxy-master1_2")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave1_1")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave1_2")
   time.sleep(5)
   stop_slave(master2)
   read_only_off(master2)
-  commands.getoutput("supervisorctl start mysql-proxy-master-failover1")
-  commands.getoutput("supervisorctl start mysql-proxy-master-failover2")
-  commands.getoutput("supervisorctl start mysql-proxy-slave-failover1")
-  commands.getoutput("supervisorctl start mysql-proxy-slave-failover2")
+  commands.getoutput("supervisorctl start mysql-proxy-master2_1")
+  commands.getoutput("supervisorctl start mysql-proxy-master2_2")
+  commands.getoutput("supervisorctl start mysql-proxy-slave2_1")
+  commands.getoutput("supervisorctl start mysql-proxy-slave2_2")
   time.sleep(5)
   while 1:
     sys.stderr.write("failover finished : master2 is new master now\n")
@@ -125,24 +129,54 @@ def failover1():
 def failover2():
   # when master2 down
   sys.stderr.write("failover2\n")
-  commands.getoutput("supervisorctl stop mysql-proxy-slave1")
-  commands.getoutput("supervisorctl stop mysql-proxy-slave2")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave1_1")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave1_2")
   time.sleep(5)
-  commands.getoutput("supervisorctl start mysql-proxy-slave-failover3")
-  commands.getoutput("supervisorctl start mysql-proxy-slave-failover4")
+  commands.getoutput("supervisorctl start mysql-proxy-slave3_1")
+  commands.getoutput("supervisorctl start mysql-proxy-slave3_2")
   time.sleep(5)
   while 1:
-    sys.stderr.write("failover finished : master2/slave4 has been purged\n")
+    sys.stderr.write("failover finished : master2/slave2 has been purged\n")
     sys.stderr.write("stop this daemon until master2 failback!\n")
     time.sleep(5)
-  
-if __name__ == '__main__':
-  master1 = conn_master1()
-  master2 = conn_master2()
-  commands.getoutput("supervisorctl start mysql-proxy-master1")
-  commands.getoutput("supervisorctl start mysql-proxy-master2")
-  commands.getoutput("supervisorctl start mysql-proxy-slave1")
-  commands.getoutput("supervisorctl start mysql-proxy-slave2")
+
+def failover3():
+  # master1 down (M2 is master)
+  sys.stderr.write("failover3(M1 down)(M2 is master)\n")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave4_1")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave4_2")
+  time.sleep(5)
+  commands.getoutput("supervisorctl start mysql-proxy-slave2_1")
+  commands.getoutput("supervisorctl start mysql-proxy-slave2_2")
+  time.sleep(5)
+  while 1:
+    sys.stderr.write("failover finished : master1/slave1 has been purged\n")
+    sys.stderr.write("stop this daemon until master1 failback!\n")
+    time.sleep(5)
+
+def failover4():
+  # when master2 down (and M2 is master)
+  sys.stderr.write("failover4(M2 master down)\n")
+  commands.getoutput("supervisorctl stop mysql-proxy-master2_1")
+  commands.getoutput("supervisorctl stop mysql-proxy-master2_2")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave4_1")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave4_2")
+  time.sleep(5)
+  commands.getoutput("supervisorctl start mysql-proxy-master1_1")
+  commands.getoutput("supervisorctl start mysql-proxy-master1_2")
+  commands.getoutput("supervisorctl start mysql-proxy-slave3_1")
+  commands.getoutput("supervisorctl start mysql-proxy-slave3_2")
+  time.sleep(5)
+  while 1:
+    sys.stderr.write("failover finished : master2/slave2 has been purged\n")
+    sys.stderr.write("stop this daemon until master2 failback!\n")
+    time.sleep(5)
+
+def watch_m1():
+  commands.getoutput("supervisorctl start mysql-proxy-master1_1")
+  commands.getoutput("supervisorctl start mysql-proxy-master1_2")
+  commands.getoutput("supervisorctl start mysql-proxy-slave1_1")
+  commands.getoutput("supervisorctl start mysql-proxy-slave1_2")
   read_only_on(master2)
   start_slave(master2)
   i = 0
@@ -178,4 +212,56 @@ if __name__ == '__main__':
     time.sleep(5)
     i += 1
 
+def watch_m2():
+  stop_slave(master1)
+  read_only_off(master1)
+  i = 0
+  while 1:
+    # check master1
+    sys.stderr.write("%d: master1 status check at %s\n" % (i, time.ctime()))
+    r = master_status(master1)
+    if r and len(r) > 0:
+      master1_err_cnt = 0
+      sys.stderr.write("ok\n")
+    else:
+      sys.stderr.write("master1 fail detected!\n")
+      master1_err_cnt += 1
+      if master1_err_cnt > 5:
+        sys.stderr.write("master1 failing: failover starting\n")
+        failover3()
+        break
+
+    # check master2
+    sys.stderr.write("%d: master2 status check at %s\n" % (i, time.ctime()))
+    r = master_status(master2)
+    if r and len(r) > 0:
+      master2_err_cnt = 0
+      sys.stderr.write("ok\n")
+    else:
+      sys.stderr.write("master2 fail detected!\n")
+      master2_err_cnt += 1
+      if master2_err_cnt > 5:
+        sys.stderr.write("master2 failing: failover starting\n")
+        failover4()
+        break
+
+    time.sleep(5)
+    i += 1
+
+  
+if __name__ == '__main__':
+  argvs = sys.argv
+  argc = len(argvs)
+  if (argc < 2):
+    print 'Usage: ha.py <m1|m2>'
+    quit()
+
+  master1 = conn_master1()
+  master2 = conn_master2()
+  if (argvs[1] == 'm1'):
+    ## when m1 is master
+    watch_m1()
+  if (argvs[1] == 'm2'):
+    ## when m2 is master
+    watch_m2()
 

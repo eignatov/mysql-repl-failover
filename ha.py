1d5da2e52a1154ef2ade3032b385ee7ecdfc31f2
utada <utada4@gmail.com>
2012-07-13 21:57:43 +0900

    modified:   ha.py

--- ha.py
+++ ha.py
@@ -61,9 +61,50 @@ def conn_master2():
                          charset="utf8",
                          cursorclass = MySQLdb.cursors.SSCursor)
 
+def conn_slave1():
+  ### edit environment
+  host     = '127.0.0.1'
+  port     = '3308'
+  username = 'root'
+  password = ''
+  dbname   = 'mysql'
+  ###
+  return MySQLdb.connect(host=host,
+                         db=dbname,
+                         user=username,
+                         passwd=password,
+                         port=int(port),
+                         charset="utf8",
+                         cursorclass = MySQLdb.cursors.SSCursor)
+
+def conn_slave2():
+  ### edit environment
+  host     = '127.0.0.1'
+  port     = '3309'
+  username = 'root'
+  password = ''
+  dbname   = 'mysql'
+  ###
+  return MySQLdb.connect(host=host,
+                         db=dbname,
+                         user=username,
+                         passwd=password,
+                         port=int(port),
+                         charset="utf8",
+                         cursorclass = MySQLdb.cursors.SSCursor)
+
 def master_status(conn):
   cur = conn.cursor()
   sql = "show master status"
+  try:
+    cur.execute(sql)
+  except:
+    return False
+  return cur.fetchall()
+
+def is_slave_running(conn):
+  cur = conn.cursor()
+  sql = "show status like 'Slave_running'"
   try:
     cur.execute(sql)
   except:
@@ -172,6 +213,48 @@ def failover4():
     sys.stderr.write("stop this daemon until master2 failback!\n")
     time.sleep(5)
 
+def slave0_error():
+  # when master2 slave is stopped
+  sys.stderr.write("slave0 error\n")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave1_1")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave1_2")
+  time.sleep(5)
+  commands.getoutput("supervisorctl start mysql-proxy-slave3_1")
+  commands.getoutput("supervisorctl start mysql-proxy-slave3_2")
+  time.sleep(5)
+  while 1:
+    sys.stderr.write("failover finished : master2/slave2 has been purged\n")
+    sys.stderr.write("stop this daemon until master2 failback!\n")
+    time.sleep(5)
+
+def slave1_error():
+  # when slave1 slave is stopped
+  sys.stderr.write("slave1 error\n")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave1_1")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave1_2")
+  time.sleep(5)
+  commands.getoutput("supervisorctl start mysql-proxy-slave5_1")
+  commands.getoutput("supervisorctl start mysql-proxy-slave5_2")
+  time.sleep(5)
+  while 1:
+    sys.stderr.write("failover finished : slave1 has been purged\n")
+    sys.stderr.write("stop this daemon until slave1 replication back!\n")
+    time.sleep(5)
+
+def slave2_error():
+  # when slave2 slave is stopped
+  sys.stderr.write("slave2 error\n")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave1_1")
+  commands.getoutput("supervisorctl stop mysql-proxy-slave1_2")
+  time.sleep(5)
+  commands.getoutput("supervisorctl start mysql-proxy-slave6_1")
+  commands.getoutput("supervisorctl start mysql-proxy-slave6_2")
+  time.sleep(5)
+  while 1:
+    sys.stderr.write("failover finished : slave2 has been purged\n")
+    sys.stderr.write("stop this daemon until slave2 replication back!\n")
+    time.sleep(5)
+
 def watch_m1():
   commands.getoutput("supervisorctl start mysql-proxy-master1_1")
   commands.getoutput("supervisorctl start mysql-proxy-master1_2")
@@ -182,7 +265,7 @@ def watch_m1():
   i = 0
   while 1:
     # check master1
-    sys.stderr.write("%d: master1 status check at %s\n" % (i, time.ctime()))
+    sys.stderr.write("%d: check master1 status at %s\n" % (i, time.ctime()))
     r = master_status(master1)
     if r and len(r) > 0:
       master1_err_cnt = 0
@@ -196,7 +279,7 @@ def watch_m1():
         break
 
     # check master2
-    sys.stderr.write("%d: master2 status check at %s\n" % (i, time.ctime()))
+    sys.stderr.write("%d: check master2 status at %s\n" % (i, time.ctime()))
     r = master_status(master2)
     if r and len(r) > 0:
       master2_err_cnt = 0
@@ -207,6 +290,54 @@ def watch_m1():
       if master2_err_cnt > 5:
         sys.stderr.write("master2 failing: failover starting\n")
         failover2()
+        break
+
+    # check replication slave at master2
+    sys.stderr.write("%d: check replication slave on master2 at %s\n" % (i, time.ctime()))
+    r = is_slave_running(master2)
+    for row in r:
+      slave_running = row[1]
+    if slave_running == "ON":
+      slave0_err_cnt = 0
+      sys.stderr.write("ok\n")
+    else:
+      sys.stderr.write("replication slave stop detected on master2!\n")
+      slave0_err_cnt += 1
+      if slave0_err_cnt > 5:
+        sys.stderr.write("replication error on master2: purging master2 from slave\n")
+        slave0_error()
+        break
+
+    # check replication slave at slave1
+    sys.stderr.write("%d: check replication slave on slave1 at %s\n" % (i, time.ctime()))
+    r = is_slave_running(slave1)
+    for row in r:
+      slave_running = row[1]
+    if slave_running == "ON":
+      slave1_err_cnt = 0
+      sys.stderr.write("ok\n")
+    else:
+      sys.stderr.write("replication slave stop detected on slave1! \n")
+      slave1_err_cnt += 1
+      if slave1_err_cnt > 5:
+        sys.stderr.write("replication error on slave1: purging slave1 from slave\n")
+        slave1_error()
+        break
+
+    # check replication slave at slave2
+    sys.stderr.write("%d: check replication slave on slave2 at %s\n" % (i, time.ctime()))
+    r = is_slave_running(slave2)
+    for row in r:
+      slave_running = row[1]
+    if slave_running == "ON":
+      slave2_err_cnt = 0
+      sys.stderr.write("ok\n")
+    else:
+      sys.stderr.write("replication slave stop detected on slave2! \n")
+      slave2_err_cnt += 1
+      if slave2_err_cnt > 5:
+        sys.stderr.write("replication error on slave2: purging slave2 from slave\n")
+        slave2_error()
         break
 
     time.sleep(5)
@@ -258,6 +389,8 @@ if __name__ == '__main__':
 
   master1 = conn_master1()
   master2 = conn_master2()
+  slave1 = conn_slave1()
+  slave2 = conn_slave2()
   if (argvs[1] == 'm1'):
     ## when m1 is master
     watch_m1()
